7c1429ef17e9aed7a8ba6d75909be1fd004bfc29
Lqwd <AppLoneQW@yandex.ru>
2025-03-10 00:49:40 +0300

    add more constants

--- src/Hangman/Main.java
+++ src/Hangman/Main.java
@@ -7,154 +7,13 @@ import java.util.*;
 
 public class Main {
 
-    final static int START = 1;
-    final static int EXIT = 2;
-    final static int MAX_MISTAKES = 6;
-
-    public static void main(String[] args) {
-        while (true){
-            printInitialScreen();
-            if (awaitInitialInput() == EXIT) System.exit(0);
-            gameInit();
-        }
-    }
-
-    private static void gameInit(){
-        int mistakeCount = 0;
-        List<Character> incorrectCharsList = new ArrayList<>();
-        List<String> listWithWords = readWordsFromFile();
-        String seekWord = getRandomWord(listWithWords);
-        String maskWord = fillingMask(seekWord);
-        do  {
-            printHangMan(mistakeCount);
-            printInputtedIncorrectChars(incorrectCharsList);
-            printMistakesCount(mistakeCount);
-            printMaskWord(maskWord);
-            String entryByPlayer = getPlayerInput();
-            if(!(isInputCorrect(entryByPlayer))){
-                printNotificationIncorrectInput();
-                continue;
-            }
-            char inputChar = getLowCaseChar(entryByPlayer);
-            if (isDuplicatedIncorrectChar(inputChar, incorrectCharsList)){
-                printNotificationDuplicatedIncorrectChar();
-            } else if (isDuplicatedCorrectChar(inputChar, maskWord)) {
-                printNotificationDuplicatedCorrectChar();
-            } else if (isWordContainsInputChar(seekWord, inputChar)){
-                maskWord = rebuildMaskWord(seekWord, inputChar, maskWord);
-            } else {
-                mistakeCount++;
-                incorrectCharsList.add(inputChar);
-            }
-
-        } while (mistakeCount < MAX_MISTAKES && !seekWord.equals(maskWord));
-        printHangMan(mistakeCount);
-        printResult(mistakeCount);
-        printInputtedIncorrectChars(incorrectCharsList);
-        printMistakesCount(mistakeCount);
-        printSeekWord(seekWord);
-    }
-
-    private static int awaitInitialInput(){
-        int entry = 0;
-        do {
-            switch (getPlayerInput()) {
-                case "старт":
-                    entry = START;
-                    break;
-                case "выход":
-                    entry = EXIT;
-                    break;
-                default:
-                    System.out.println("Уведомление: Некорректный ввод!");
-                    System.out.println("Ожидается 'старт' или 'выход'");
-                    System.out.println("Ввод должн быть без одинарных кавычек");
-            }
-        } while (entry != START && entry != EXIT);
-        return entry;
-    }
-
-    private static String getPlayerInput(){
-        Scanner scanner = new Scanner(System.in);
-        return scanner.nextLine();
-    }
-
-    private static List<String> readWordsFromFile(){
-        List<String> listWithWords = new ArrayList<>();
-        try {
-            FileReader filePath = new FileReader("src/Hangman/WordsNouns.txt");
-            BufferedReader reader = new BufferedReader(filePath);
-            String line = reader.readLine();
-            while (line != null) {
-                listWithWords.add(line);
-                line = reader.readLine();
-            }
-            reader.close();
-        } catch (IOException e) {
-            System.err.println(e.getMessage());
-        }
-        return listWithWords;
-    }
-
-    private static String getRandomWord(List<String> listWithWords) {
-        int numberLine = getRandInt(listWithWords.size());
-        return listWithWords.get(numberLine);
-    }
-
-    private static int getRandInt (int linesNumber){
-        Random rand = new Random();
-        return rand.nextInt(linesNumber);
-    }
-
-    private static String fillingMask(String seekWord){
-        return "*".repeat(seekWord.length());
-    }
-
-    private static boolean isInputCorrect(String PlayerInput){
-        return PlayerInput.length() == 1 && !isSymbolNotCyrillic(PlayerInput.charAt(0));
-    }
-
-    private static boolean isSymbolNotCyrillic(char inputChar){
-        return Character.UnicodeBlock.of(inputChar) != Character.UnicodeBlock.CYRILLIC;
-    }
-
-    private static boolean isDuplicatedIncorrectChar(char inputChar, List<Character> incorrectCharsList){
-        return incorrectCharsList.contains(inputChar);
-    }
-
-    private static boolean isDuplicatedCorrectChar(char inputChar, String maskWord){
-        return maskWord.indexOf(inputChar) != -1;
-    }
-    private static char getLowCaseChar(String playerInput){
-        return Character.toLowerCase(playerInput.charAt(0));
-    }
-
-    private static boolean isWordContainsInputChar(String seekWord, char inputChar){
-        return seekWord.indexOf(inputChar) != -1;
-    }
-
-    private static String rebuildMaskWord(String seekWord, char inputChar, String maskWord){
-        StringBuilder tempString = new StringBuilder(maskWord);
-        for(int i = 0; i < seekWord.length(); i++) {
-            if (seekWord.charAt(i) == inputChar) {
-                tempString.setCharAt(i, inputChar);
-            }
-        }
-        maskWord = tempString.toString();
-        return maskWord;
-    }
-
-    private static void printInitialScreen (){
-        System.out.println();
-        System.out.println("|------------------------------Виселица-----------------------------|");
-        System.out.println("|Для начала игры введите 'старт', для выхода из игры введите 'выход'|");
-        System.out.println("|Ввод должн быть без одинарных кавычек                              |");
-        System.out.println("|-------------------------------------------------------------------|");
-    }
-
-    private static void  printHangMan (int mistakeCount) {
-        String[][] versionOfHangMan = {
-                {"""
+    private final static String CMD_START = "старт";
+    private final static String CMD_EXIT = "выход";
+    private final static int MAX_MISTAKES = 6;
+    private static final Random RANDOM = new Random();
+    private static final Scanner SCANNER = new Scanner(System.in);
+    private static final String[][] versionOfHangMan = {
+            {"""
                      +--------+
                      |/
                      |
@@ -163,8 +22,7 @@ public class Main {
                      |
                      |
                  ____|____
-                 """},
-                {"""
+                 """}, {"""
                      +--------+
                      |/       |
                      |       (_)
@@ -174,7 +32,7 @@ public class Main {
                      |
                  ____|____
                  """},
-                {"""
+    {"""
                      +--------+
                      |/       |
                      |       (_)
@@ -184,7 +42,7 @@ public class Main {
                      |
                  ____|____
                  """},
-                {"""
+    {"""
                      +--------+
                      |/       |
                      |       (_)
@@ -194,7 +52,7 @@ public class Main {
                      |
                  ____|____
                  """},
-                {"""
+    {"""
                      +--------+
                      |/       |
                      |       (_)
@@ -204,7 +62,7 @@ public class Main {
                      |
                  ____|____
                  """},
-                {"""
+    {"""
                      +--------+
                      |/       |
                      |       (_)
@@ -214,7 +72,7 @@ public class Main {
                      |
                  ____|____
                  """},
-                {"""
+    {"""
                      +--------+
                      |/       |
                      |       (_)
@@ -224,25 +82,160 @@ public class Main {
                      |
                  ____|____
                  """}
-        };
+    };
+
+    public static void main(String[] args) {
+        while (true){
+            printInitialScreen();
+            if (awaitInitialInput().equals(CMD_EXIT)) System.exit(0);
+            start();
+        }
+    }
+
+    private static void start(){
+        int mistakeCount = 0;
+        boolean isGameOver = false;
+        List<Character> wrongLetters = new ArrayList<>();
+        List<String> words = readWords();
+        String seekWord = getRandomWord(words);
+        String maskWord = fillingMask(seekWord);
+        do  {
+            printHangMan(mistakeCount);
+            showWrongLetters(wrongLetters);
+            printMistakesCount(mistakeCount);
+            printMaskWord(maskWord);
+            String entryByPlayer = getPlayerInput();
+            if(!(isOneLetter(entryByPlayer))){
+                notifyWrongInput();
+                continue;
+            }
+            char inputLetter = getLetter(entryByPlayer);
+            if (isWrongDuplicated(inputLetter, wrongLetters)){
+                notifyWrongsDuplicated();
+            } else if (isCorrectDuplicated(inputLetter, maskWord)) {
+                notifyCorrectDuplicated();
+            } else if (isCorrectLetter(seekWord, inputLetter)){
+                maskWord = rebuildMask(seekWord, inputLetter, maskWord);
+            } else {
+                mistakeCount++;
+                wrongLetters.add(inputLetter);
+            }
+            isGameOver = mistakeCount >= MAX_MISTAKES || seekWord.equals(maskWord);
+        } while (!isGameOver);
+        printHangMan(mistakeCount);
+        printResult(mistakeCount);
+        showWrongLetters(wrongLetters);
+        printMistakesCount(mistakeCount);
+        printSeekWord(seekWord);
+    }
+
+    private static String awaitInitialInput(){
+        while(true) {
+            switch (getPlayerInput()) {
+                case "старт":
+                    return CMD_START;
+                case "выход":
+                    return CMD_EXIT;
+                default:
+                    System.out.println("Уведомление: Некорректный ввод!");
+                    System.out.println("Ожидается 'старт' или 'выход'");
+                    System.out.println("Ввод должн быть без одинарных кавычек");
+            }
+        }
+    }
+
+    private static String getPlayerInput(){
+        return SCANNER.nextLine().toLowerCase();
+    }
+
+    private static List<String> readWords(){
+        List<String> words = new ArrayList<>();
+        try {
+            FileReader filePath = new FileReader("src/Hangman/WordsNouns.txt");
+            BufferedReader reader = new BufferedReader(filePath);
+            String line = reader.readLine();
+            while (line != null) {
+                words.add(line);
+                line = reader.readLine();
+            }
+            reader.close();
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+        return words;
+    }
+
+    private static String getRandomWord(List<String> words) {
+        int size = words.size();
+        int numberLine = RANDOM.nextInt(size);
+        return words.get(numberLine);
+    }
+
+    private static String fillingMask(String seekWord){
+        return "*".repeat(seekWord.length());
+    }
+
+    private static boolean isOneLetter(String s){
+        return s.length() == 1 && isCyrillic(s.charAt(0));
+    }
+
+    private static boolean isCyrillic(char letter){
+        return Character.UnicodeBlock.of(letter) == Character.UnicodeBlock.CYRILLIC;
+    }
+
+    private static boolean isWrongDuplicated(char inputChar, List<Character> incorrectLetters){
+        return incorrectLetters.contains(inputChar);
+    }
+
+    private static boolean isCorrectDuplicated(char inputChar, String maskWord){
+        return maskWord.indexOf(inputChar) != -1;
+    }
+
+    private static char getLetter(String playerInput){
+        return playerInput.charAt(0);
+    }
+
+    private static boolean isCorrectLetter(String seekWord, char inputChar){
+        return seekWord.indexOf(inputChar) != -1;
+    }
+
+    private static String rebuildMask(String seekWord, char inputChar, String mask){
+        StringBuilder result = new StringBuilder(mask);
+        for(int i = 0; i < seekWord.length(); i++) {
+            if (seekWord.charAt(i) == inputChar) {
+                result.setCharAt(i, inputChar);
+            }
+        }
+        return result.toString();
+    }
+
+    private static void printInitialScreen (){
+        System.out.println();
+        System.out.println("|------------------------------Виселица-----------------------------|");
+        System.out.printf("|Для начала игры введите '%s', для выхода из игры введите '%s'| %n", CMD_START, CMD_EXIT);
+        System.out.println("|Ввод должн быть без одинарных кавычек                              |");
+        System.out.println("|-------------------------------------------------------------------|");
+    }
+
+    private static void  printHangMan (int mistakeCount) {
         System.out.print(String.join("", versionOfHangMan[mistakeCount]));
     }
 
-    private static void printNotificationIncorrectInput (){
+    private static void notifyWrongInput(){
         System.out.println("Уведомление: Некорректный ввод!");
         System.out.println("Ожидается ввод только одной буквы кириллицы");
     }
 
-    private static void printNotificationDuplicatedIncorrectChar (){
+    private static void notifyWrongsDuplicated(){
         System.out.println("Уведомление: повторно введена некорректная буква!");
     }
 
-    private static void printNotificationDuplicatedCorrectChar (){
+    private static void notifyCorrectDuplicated(){
         System.out.println("Уведомление: повторно введена угаданная буква!");
     }
 
-    private static void printInputtedIncorrectChars(List<Character> incorrectCharsList){
-        System.out.println("Неправильно введенные буквы:" + incorrectCharsList);
+    private static void showWrongLetters(List<Character> wrongLetters){
+        System.out.println("Неправильно введенные буквы:" + wrongLetters);
     }
 
     private static void printMistakesCount (int mistakeCount){
